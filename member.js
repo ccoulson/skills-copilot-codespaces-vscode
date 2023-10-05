307c74bdccb8abaeeb3dc6abf925f16104b48fd5
ccoulson <ccoulson@visigo.com>
2023-10-05 19:48:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  if (member.style.display === "block") {
+    member.style.display = "none";
+  } else {
+    member.style.display = "block";
+  }
+}
